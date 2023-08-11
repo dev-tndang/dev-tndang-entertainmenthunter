3e43dd5742eac39ccb9466511018a62115f1d9dd
Thien Dang <thien.dang@mykenzie.snhu.edu>
2023-08-11 10:48:59 -0400

    Update 2023.08.11.11
    Reseted project by deleting src files and starting from scratch. Firstly creating an Index.js, then a App js that are linked to one another

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-import { useState, useEffect } from 'react';
-import './App.css';
+import React from "react";
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
-
-  useEffect(() => {
-    alert("The counter has been set to: " + counter);
-  }, [counter])
-
-  return (
-    <div className="App">
-      <h1>
-        <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
-        {counter}
-        <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
-      </h1>
-    </div>
-  );
+    return (
+        <h1>App</h1>
+    )
 }
 
 export default App;
