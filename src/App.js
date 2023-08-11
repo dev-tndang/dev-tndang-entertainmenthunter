55e37c1a29d26e0305a48c7b5b3cb3a582c5e6fd
Thien Dang <thien.dang@mykenzie.snhu.edu>
2023-08-11 11:26:21 -0400

    Update 2023.08.11.12
    Using the Omdb API, added an asynchronous searchMovie function to fetch a search using an assigned API Key. Then to have a test search active at start and logging the results to the console at start using the useEffect hook from the React library

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import React from "react";
+import { useEffect } from "react";
+
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=b7ef72a5";
 
 const App = () => {
+    const searchMovie = async(title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data);
+    }
+
+    useEffect(() => {
+        searchMovie("Spiderman");
+    }, [])
+
     return (
         <h1>App</h1>
     )
