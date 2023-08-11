354921742a8606f981c5a600845db9af88b93f63
Thien Dang <thien.dang@mykenzie.snhu.edu>
2023-08-11 09:42:29 -0400

    Update 2023.08.11.07
    Dynamically interact with the Person component from the App component via "props" arguments

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 
-const Person = () => {
+const Person = (props) => {
   return (
     <>
-      <h2>First Name: Thien</h2>
-      <h2>Last Name: Dang</h2>
-      <h3>Age: 31</h3>
+      <h2>First Name: {props.firstName}</h2>
+      <h2>Last Name: {props.lastName}</h2>
+      <h3>Age: {props.age}</h3>
     </>
   )
 }
@@ -13,7 +13,21 @@ const Person = () => {
 const App = () => {
   return (
     <div className="App">
-      <Person />
+      <Person 
+        firstName='Thien'
+        lastName='Dang'
+        age='31'
+      />
+      <Person 
+        firstName='Kirigaya'
+        lastName='Kazuto'
+        age='21'
+      />
+      <Person 
+        firstName='Percy'
+        lastName='Bearinton'
+        age='43'
+      />
     </div>
   );
 }
