86053976b3e84053da59229b7a864cc8aaaca7be
Thien Dang <thien.dang@mykenzie.snhu.edu>
2023-08-11 08:58:13 -0400

    Update 2023.08.11.03
    Cleared text, removed default React Logo, changed the App function into a const arrow function, and rendered an h1 element of "Hello React"

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>This is a test of addition of: 6 + 9 = {6+9}</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello React!</h1>
     </div>
   );
 }
