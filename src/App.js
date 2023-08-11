927594959e03e68d6a23249e96e8dd924a142d6d
Thien Dang <thien.dang@mykenzie.snhu.edu>
2023-08-11 12:23:09 -0400

    Update 2023.08.11.15
    To test the rendering created an Object based from the console log results of the useEffect. Then created a container classed div below the search classed div that renders a card  with a movie classed div. The card would show the movie's year, poster, type, and title. There is an tetiary if statement if no poster is available to default to an online placeholder source

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import SearchIcon from "./search.svg";
 
 const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=b7ef72a5";
 
+const movie1 = {
+    "Poster": "https://m.media-amazon.com/images/M/MV5BZWQxMjcwNjItZjI0ZC00ZTc4LWIwMzItM2Q0YTZhNzI3NzdlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_SX300.jpg",
+    "Title": "Italian Spiderman",
+    "Type": "movie",
+    "Year": "2007",
+    "imdbID": "tt2705436",
+}
+
 const App = () => {
     const searchMovie = async(title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
@@ -20,6 +28,7 @@ const App = () => {
     return (
         <div className="app">
             <h1>Movie Base</h1>
+
             <div className="search">
                 <input
                     placeholder="Search for a Movie..."
@@ -31,6 +40,25 @@ const App = () => {
                     alt="search"
                     onClick={() => {}}
                 />
+            </div>
+
+            <div className="container">
+                <div className="movie">
+                    <div>
+                        <p>{movie1.Year}</p>
+                    </div>
+                    <div>
+                        <img
+                            src={movie1.Poster !== "N/A" ? movie1.Poster : "https://via.placeholder.com/400"}
+                            alt={movie1.Title}
+                        />
+                    </div>
+                    <div>
+                        <span>{movie1.Type}</span>
+                        <h3>{movie1.Title}</h3>
+                    </div>
+
+                </div>
             </div>
         </div>
     )
