a4ec0233e3fc43c051c4a42e06c6b3b33309f6f2
Thien Dang <thien.dang@mykenzie.snhu.edu>
2023-08-11 08:53:42 -0400

    Update 2023.08.11.02
    Testing the App.js writing JS elements into HTML via a sum example

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>This is a test of addition of: 6 + 9 = {6+9}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
