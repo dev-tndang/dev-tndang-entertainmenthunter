9ccf59410e1d9bbd58f4c396432eddf3d84e3f5d
Thien Dang <thien.dang@mykenzie.snhu.edu>
2023-08-11 09:52:03 -0400

    Update 2023.08.11.08
    Imported useState from React, with early implementation of a counter State will change when pressing the plus or minus buttons

--- src/App.js
+++ src/App.js
@@ -1,33 +1,15 @@
+import { useState } from 'react';
 import './App.css';
 
-const Person = (props) => {
-  return (
-    <>
-      <h2>First Name: {props.firstName}</h2>
-      <h2>Last Name: {props.lastName}</h2>
-      <h3>Age: {props.age}</h3>
-    </>
-  )
-}
-
 const App = () => {
+  const [counter, setCounter] = useState(0);
   return (
     <div className="App">
-      <Person 
-        firstName='Thien'
-        lastName='Dang'
-        age='31'
-      />
-      <Person 
-        firstName='Kirigaya'
-        lastName='Kazuto'
-        age='21'
-      />
-      <Person 
-        firstName='Percy'
-        lastName='Bearinton'
-        age='43'
-      />
+      <h1>
+        <button>-</button>
+        {counter}
+        <button>+</button>
+      </h1>
     </div>
   );
 }
